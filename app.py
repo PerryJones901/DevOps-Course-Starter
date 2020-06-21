fe0349f33d108e9e353432812b370cdcda03541a
Perry Jones <perry.jones@softwire.com>
2020-06-21 11:30:35 +0100

    Ex2: App now has some methods from Trello Helper.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import session_items as session
+import trello_helper as trello
 
 app = Flask(__name__)
 app.config.from_object('flask_config.Config')
@@ -11,7 +12,7 @@ def toggle_string(status_string):
 
 @app.route('/index')
 def index():
-    items = session.get_items()
+    items = trello.get_cards()
     return render_template('index.html', items=items)
 
 @app.route('/sorted/<field>')
@@ -21,20 +22,22 @@ def sorted_by(field):
 
 @app.route('/add', methods=['POST'])
 def add_something():
-    session.add_item(request.form.get('title'))
+    trello.add_card(request.form.get('title'))
     return redirect(url_for('index'))
 
 @app.route('/toggle/<int:id>', methods=['POST'])
 def update_something(id):
-    item = session.get_item(id)
+    item = trello.get_card(id)
     if item is not None:
-        item['status'] = toggle_string(item['status'])
-        session.save_item(item)
+        if item.is_complete:
+            trello.mark_card_todo(id)
+        else:
+            trello.mark_card_complete(id)
     return redirect(url_for('index'))
 
 @app.route('/delete/<int:id>', methods=['POST'])
 def delete_item(id):
-    session.delete_item(id)
+    trello.delete_card(id)
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
