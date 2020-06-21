47917a0b15ab626657dc49da13b2aeed52cd3e70
Perry Jones <perry.jones@softwire.com>
2020-06-21 11:29:33 +0100

    Ex2: Trello Helper Refactoring and added 'mark as complete' and delete card.

--- trello_helper.py
+++ trello_helper.py
@@ -20,21 +20,26 @@ def is_done_list(listId):
 def json_to_card(json):
     id = json['id']
     title = json['name']
-    is_complete = is_done_list(json['listId'])
+    is_complete = is_done_list(json['idList'])
     return Card(id=id, title=title, is_complete=is_complete)
 
-def key_and_token():
-    return {"key": KEY, "token": TOKEN}
+def params_with_auth(params):
+    return {"key": KEY, "token": TOKEN, **params}
 
-def get_from_board(uri, extra_params={}):
-    url = f"{BASE_URL}/boards/{BOARD_ID}{uri}"
-    params = key_and_token().update(extra_params)
-    return requests.get(url, params=params).json()
+def build_url(uri):
+    return f"{BASE_URL}/boards/{BOARD_ID}{uri}"
 
-def post_to_board(uri, extra_params={}):
-    url = f"{BASE_URL}/boards/{BOARD_ID}{uri}"
-    params = key_and_token().update(extra_params)
-    return requests.post(url, params=params).json()
+def get_from_board(uri, params={}):
+    return requests.get(build_url(uri), params=params_with_auth(params)).json()
+
+def post_to_board(uri, params={}):
+    return requests.post(build_url(uri), params=params_with_auth(params)).json()
+
+def put_to_board(uri, params={}):
+    return requests.put(build_url(uri), params=params_with_auth(params)).json()
+
+def delete_from_board(uri, params={}):
+    return requests.delete(build_url(uri), params=params_with_auth(params)).json()
 
 def get_board():
     return get_from_board("")
@@ -44,13 +49,23 @@ def get_cards():
     return [json_to_card(item) for item in response_json]
 
 def get_card(id):
-    response_json = get_from_board("/cards/{id}")
+    response_json = get_from_board(f"/cards/{id}")
     return json_to_card(response_json)
 
 def get_lists():
     return get_from_board("/lists")
 
-def add_card_to_leftmost_list(name='ToDo List item', desc='This needs completing!'):
-    leftmost_list_id = int(get_lists()[0]['id'])
-    extra_params = {"name": name, "desc": desc, "listId": leftmost_list_id}
-    return post_to_board("/cards", extra_params=extra_params)
+def add_card(name='ToDo List item'):
+    params = {"name": name, "listId": TODO_LIST_ID}
+    return post_to_board("/cards", params=params)
+
+def mark_card_todo(id):
+    params = {"listId": TODO_LIST_ID}
+    return put_to_board(f"/cards/{id}", params=params)
+
+def mark_card_complete(id):
+    params = {"listId": DONE_LIST_ID}
+    return put_to_board(f"/cards/{id}", params=params)
+
+def delete_card(id):
+    return delete_from_board(f"/cards/{id}")
