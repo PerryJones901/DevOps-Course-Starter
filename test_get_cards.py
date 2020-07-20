b7c69493815f11033012c69b72d9279481063ecc
Perry Jones <perry.jones@softwire.com>
2020-07-20 23:31:49 +0100

    Ex3: Added new test for assigning cards to correct lists.

--- test_get_cards.py
+++ test_get_cards.py
@@ -17,7 +17,7 @@ def cards():
     card4 = Card(4, 4, "Another Task in Todo List", None, LISTS[0].id)
     return [card1, card2, card3, card4]
 
-def test_keeps_list_order(cards):
+def test_view_model_keeps_list_order(cards):
     # Act
     view_model = ViewModel(cards, LISTS)
 
@@ -25,3 +25,13 @@ def test_keeps_list_order(cards):
     assert view_model.lists[0].id == 101
     assert view_model.lists[1].id == 102
     assert view_model.lists[2].id == 103
+
+def test_view_model_assigns_cards_to_correct_lists(cards):
+    # Act
+    view_model = ViewModel(cards, LISTS)
+
+    # Assert
+    assert view_model.lists[0].cards[0].id == 1
+    assert view_model.lists[0].cards[1].id == 4
+    assert view_model.lists[1].cards[0].id == 2
+    assert view_model.lists[2].cards[0].id == 3
