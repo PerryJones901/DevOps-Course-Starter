bcb0f8488ad1248768b3dcabdd7bb461a413ebf5
Perry Jones <perry.jones@softwire.com>
2020-07-29 16:41:10 +0100

    Ex3: Removed redundant 'Sort by Status'

--- api/sorter.py
+++ api/sorter.py
@@ -4,7 +4,6 @@ def sort_lambda(field):
     return {
         'id' : lambda item: item.idShort,
         'title' : lambda item: item.title,
-        'List' : lambda item: item.list_id
     }.get(field, lambda item: item.idShort)
 
 def sort_cards(cards, field):
