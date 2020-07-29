15a261c101f99dbb94320e1c165c240e2d9c5d5b
Perry Jones <perry.jones@softwire.com>
2020-07-29 13:55:09 +0100

    Ex3: Integration Test added.

--- app.py
+++ app.py
@@ -5,48 +5,51 @@ import sorter as sorter
 import trello_helper as trello
 from view_model import ViewModel
 
-app = Flask(__name__)
-app.config.from_object('flask_config.Config')
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('flask_config.Config')
 
-@app.route('/')
-def index():
-    cards = trello.get_cards()
-    sort_by_field = session.get_sort_by_field()
-    items = sorter.sort_cards(cards, sort_by_field)
-    lists = trello.get_lists()
-    show_all = session.get_show_all_completed_tasks()
-    item_view_model = ViewModel(items, lists, show_all, datetime.now().date())
-    return render_template('index.html', view_model=item_view_model)
+    @app.route('/')
+    def index():
+        cards = trello.get_cards()
+        sort_by_field = session.get_sort_by_field()
+        items = sorter.sort_cards(cards, sort_by_field)
+        lists = trello.get_lists()
+        show_all = session.get_show_all_completed_tasks()
+        item_view_model = ViewModel(items, lists, show_all, datetime.now().date())
+        return render_template('index.html', view_model=item_view_model)
 
-@app.route('/sorted/<field>')
-def sorted_by(field):
-    session.change_sort_by_field(field)
-    return redirect(url_for('index'))
+    @app.route('/sorted/<field>')
+    def sorted_by(field):
+        session.change_sort_by_field(field)
+        return redirect(url_for('index'))
 
-@app.route('/show_all')
-def show_all_completed_tasks():
-    session.toggle_show_all_completed_tasks()
-    return redirect(url_for('index'))
+    @app.route('/show_all')
+    def show_all_completed_tasks():
+        session.toggle_show_all_completed_tasks()
+        return redirect(url_for('index'))
 
-@app.route('/add', methods=['POST'])
-def add_card_to_list():
-    title = request.form.get('title')
-    list_name = request.form.get('card_list')
-    due = request.form.get('due')
-    card_list = trello.get_list(list_name)
-    trello.add_card(title, card_list.id, due)
-    return redirect(url_for('index'))
+    @app.route('/add', methods=['POST'])
+    def add_card_to_list():
+        title = request.form.get('title')
+        list_name = request.form.get('card_list')
+        due = request.form.get('due')
+        card_list = trello.get_list(list_name)
+        trello.add_card(title, card_list.id, due)
+        return redirect(url_for('index'))
 
-@app.route('/card/<card_id>', methods=['POST'])
-def move_card_to_list(card_id):
-    list_id = request.form.get('card_list')
-    trello.move_card_to_list(card_id, list_id)
-    return redirect(url_for('index'))
+    @app.route('/card/<card_id>', methods=['POST'])
+    def move_card_to_list(card_id):
+        list_id = request.form.get('card_list')
+        trello.move_card_to_list(card_id, list_id)
+        return redirect(url_for('index'))
 
-@app.route('/delete/<card_id>', methods=['POST'])
-def delete_item(card_id):
-    trello.delete_card(card_id)
-    return redirect(url_for('index'))
+    @app.route('/delete/<card_id>', methods=['POST'])
+    def delete_item(card_id):
+        trello.delete_card(card_id)
+        return redirect(url_for('index'))
 
-if __name__ == '__main__':
-    app.run()
+    if __name__ == '__main__':
+        app.run()
+
+    return app
---
+++ test_integration.py
@@ -0,0 +1,47 @@
+import os
+from datetime import datetime, timedelta
+from unittest import mock
+
+import pytest
+from dotenv import find_dotenv, load_dotenv
+
+import app
+import env_vars as env
+import test_constants as const
+from card import Card
+from card_list import CardList
+from mock_response import MockResponse
+from view_model import ViewModel
+
+@pytest.fixture
+def client():
+    # Use our test integration config instead of the 'real' version
+    file_path = find_dotenv('.env.test')
+    load_dotenv(file_path, override=True)
+
+    # Create the new app.
+    test_app = app.create_app()
+
+    # Use the app to create a test_client that can be used in our tests.
+    with test_app.test_client() as client:
+        yield client
+
+def params_url_string(params):
+    params = {"key": env.KEY, "token": env.TOKEN, **params}
+    params_url_suffix = '?'
+    for (key, value) in params:
+        params_url_suffix = params_url_suffix + f'{key}={value}'
+    return params_url_suffix
+
+def mock_get_requests(*args, **kwargs):
+    if args[0] == f'https://api.trello.com/1/boards/{env.BOARD_ID}/cards':
+        return MockResponse(const.CARD_ARR, 200)
+    elif args[0] == f'https://api.trello.com/1/boards/{env.BOARD_ID}/lists':
+        return MockResponse(const.LIST_ARR, 200)
+
+    return MockResponse(None, 404)
+
+@mock.patch('requests.get', side_effect=mock_get_requests)
+def test_index_page(mock_get_requests, client):
+    response = client.get('/')
+    assert response.status_code == 200
