c74ead0b13ff0f9626850c82a7926bddbf2b0fc5
Perry Jones <perry.jones@softwire.com>
2020-12-23 17:10:33 +0000

    Ex8: Docker deploy logic moved to bash script

---
+++ .travis_docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -ev
+
+TARGET_IMAGE="${DOCKER_USERNAME}/${DOCKER_REPO}"
+TARGET_IMAGE_LATEST="${TARGET_IMAGE}:latest"
+TARGET_IMAGE_VERSIONED="${TARGET_IMAGE}:${TRAVIS_COMMIT}"
+
+docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
+docker build \
+ --target production \
+ --tag ${TARGET_IMAGE_LATEST} \
+ --tag ${TARGET_IMAGE_VERSIONED} .
+docker push ${TARGET_IMAGE_LATEST}
+docker push ${TARGET_IMAGE_VERSIONED}
