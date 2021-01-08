3f9664e070fb18ca35c91b42700e0836e24cc61d
Perry Jones <perry.jones@softwire.com>
2021-01-08 12:54:49 +0000

    Ex8: Push to Heroku registry

--- .travis_docker.sh
+++ .travis_docker.sh
@@ -4,6 +4,9 @@ set -ev
 TARGET_IMAGE="${DOCKER_USERNAME}/${DOCKER_REPO}"
 TARGET_IMAGE_LATEST="${TARGET_IMAGE}:latest"
 TARGET_IMAGE_VERSIONED="${TARGET_IMAGE}:${TRAVIS_COMMIT}"
+HEROKU_APP_REGISTRY_URL="registry.heroku.com/${HEROKU_APP_NAME}/web"
+
+# Push to Docker Hub
 
 docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
 docker build \
@@ -12,3 +15,10 @@ docker build \
  --tag ${TARGET_IMAGE_VERSIONED} .
 docker push ${TARGET_IMAGE_LATEST}
 docker push ${TARGET_IMAGE_VERSIONED}
+
+# Push to Heroku Registry
+
+docker login --username=_ --password=${HEROKU_AUTH_TOKEN} registry.heroku.com
+docker pull $TARGET_IMAGE
+docker tag $TARGET_IMAGE $HEROKU_APP_REGISTRY_URL
+docker push $HEROKU_APP_REGISTRY_URL
