6a0cf5014b1c6b011e8ee2b43594cff498cfde80
Perry Jones <perry.jones@softwire.com>
2021-04-13 22:26:55 +0100

    Ex11: Removed Heroku deploy for Azure deploy

--- .travis_docker.sh
+++ .travis_docker.sh
@@ -4,7 +4,6 @@ set -ev
 TARGET_IMAGE="${DOCKER_USERNAME}/${DOCKER_REPO}"
 TARGET_IMAGE_LATEST="${TARGET_IMAGE}:latest"
 TARGET_IMAGE_VERSIONED="${TARGET_IMAGE}:${TRAVIS_COMMIT}"
-HEROKU_APP_REGISTRY_URL="registry.heroku.com/${HEROKU_APP_NAME}/web"
 
 # Push to Docker Hub
 
@@ -16,13 +15,6 @@ docker build \
 docker push ${TARGET_IMAGE_LATEST}
 docker push ${TARGET_IMAGE_VERSIONED}
 
-# Push to Heroku Registry
+# POST to Deploy Webhook
 
-echo $HEROKU_AUTH_TOKEN | docker login --username=_ --password-stdin registry.heroku.com
-docker pull $TARGET_IMAGE
-docker tag $TARGET_IMAGE $HEROKU_APP_REGISTRY_URL
-docker push $HEROKU_APP_REGISTRY_URL
-
-# Release
-curl https://cli-assets.heroku.com/install.sh | sh
-heroku container:release web --app=$HEROKU_APP_NAME
+curl -dH -X POST "${DEPLOY_WEBHOOK_URL}"
--- api/app_config.py
+++ api/app_config.py
@@ -2,28 +2,18 @@ import os
 
 class AppConfig:
     def __init__(self):
-        self._MONGO_USERNAME = os.environ.get('MONGO_USERNAME')
-        self._MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD')
-        self._MONGO_CLUSTER_NAME = os.environ.get('MONGO_CLUSTER_NAME')
-        self._MONGO_DEFAULT_DB_NAME = os.environ.get('MONGO_DB_NAME')
+        self._MONGO_DB_NAME = os.environ.get('MONGO_DB_NAME')
+        self._MONGO_CONNECTION_STRING = os.environ.get('MONGO_CONNECTION_STRING')
         self._SECRET_KEY = os.environ.get('SECRET_KEY')
         self._LOGIN_DISABLED = os.environ.get('LOGIN_DISABLED', None)
 
     @property
-    def MONGO_USERNAME(self) -> str:
-        return self._MONGO_USERNAME
-
-    @property
-    def MONGO_PASSWORD(self) -> str:
-        return self._MONGO_PASSWORD
-
-    @property
-    def MONGO_CLUSTER_NAME(self) -> str:
-        return self._MONGO_CLUSTER_NAME
-
-    @property
     def MONGO_DB_NAME(self) -> str:
-        return self._MONGO_DEFAULT_DB_NAME
+        return self._MONGO_DB_NAME
+
+    @property
+    def MONGO_CONNECTION_STRING(self) -> str:
+        return self._MONGO_CONNECTION_STRING
 
     @property
     def SECRET_KEY(self) -> str:
