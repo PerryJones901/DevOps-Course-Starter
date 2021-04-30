edf4c635144ea68ff45b1a400219d037da566a91
Perry Jones <perry.jones@softwire.com>
2021-04-30 16:28:32 +0100

    Ex12: Added Cosmos Config

--- main.tf
+++ main.tf
@@ -6,9 +6,11 @@ terraform {
     }
   }
 }
+
 provider "azurerm" {
   features {}
 }
+
 data "azurerm_resource_group" "main" {
   name = "SoftwirePilot_PerryJones_ProjectExercise"
 }
@@ -25,6 +27,33 @@ resource "azurerm_app_service_plan" "main" {
   }
 }
 
+resource "azurerm_cosmosdb_account" "main" {
+  name                = "ppp-cosmosdb-account-perjon"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  offer_type          = "Standard"
+  kind                = "MongoDB"
+
+  capabilities {
+    name = "EnableMongo"
+  }
+
+  capabilities {
+    name = "EnableServerless"
+  }  
+
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 10
+    max_staleness_prefix    = 200
+  }
+
+  geo_location {
+    location          = data.azurerm_resource_group.main.location
+    failover_priority = 0
+  }
+}
+
 resource "azurerm_app_service" "main" {
   name = "perfect-productivity-platform"
   location = data.azurerm_resource_group.main.location
@@ -32,9 +61,21 @@ resource "azurerm_app_service" "main" {
   app_service_plan_id = azurerm_app_service_plan.main.id
   site_config {
     app_command_line = ""
-    linux_fx_version = "DOCKER|appsvcsample/python-helloworld:latest"
+    linux_fx_version = "DOCKER|perryjones/ppp:latest"
   }
   app_settings = {
    "DOCKER_REGISTRY_SERVER_URL" = "https://index.docker.io"
+   "MONGODB_CONNECTION_STRING" = "mongodb://${azurerm_cosmosdb_account.main.name}:${azurerm_cosmosdb_account.main.primary_key}@${azurerm_cosmosdb_account.main.name}.mongo.cosmos.azure.com:10255/DefaultDatabase?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000"
   }
 }
+
+resource "azurerm_cosmosdb_mongo_database" "main" {
+  name                = "ppp-cosmos-mongo-db"
+  resource_group_name = azurerm_cosmosdb_account.main.resource_group_name
+  account_name        = azurerm_cosmosdb_account.main.name
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
